ca4e80927e9465fbf00ec301648bffa5b0d1c219
s-laul <54870014+s-laul@users.noreply.github.com>
2023-01-10 22:16:02 -0500

    button clicking and disabling

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert("hello world!")
+const moodButtons = document.querySelectorAll(".mood-buttons")
+
+moodButtons.forEach(button => {
+   button.addEventListener("click", () => {
+      moodButtons.forEach (btn => {
+         btn.disabled = true;
+      });
+      button.style.backgroundColor = "beige"
+   })
+})
+
+const timeButtons = document.querySelectorAll(".time-buttons")
+
+timeButtons.forEach(button => {
+   button.addEventListener("click", () => {
+      timeButtons.forEach (btn => {
+         btn.disabled = true;
+      });
+      button.style.backgroundColor = "beige"
+   })
+})
+
+
+
