71e99fe584c2f55ffc4f919e230d4b8031aabfa4
s-laul <54870014+s-laul@users.noreply.github.com>
2023-01-14 16:33:01 -0500

    background colors triggered by mood and time button click

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # raga-rasa
 GA Project 1
+
+This project uses basic HTML, CSS, and JavaScript in order to match a user's mood  based on the time of day through visual and aural stimuli. 
+
+The visual element utilizes curated gradients based on color theory to match a user's inputs through a dynamic CSS Gradient Animator (Source: <a href="https://www.gradient-animator.com/" target="_blank">Ian Forrest</a>).
+
+The aural element utilizes individually-selected <a href="https://en.wikipedia.org/wiki/Raga" target="_blank">_ragas_ (Sanksrit for 'color')</a>, or Indian classical music frameworks, deliberately composed to invoke, passify, and accentuate a listener's mood (<a href="https://www.frontiersin.org/articles/10.3389/fpsyg.2015.00513/full#:~:text=The%20ragas%20with%20emotion%20labels,Todi%2C%20Basant%20Mukhari%2C%20Lalit."> Mathur et. al, 2015</a>) (Sanskrit _rasa_) based on time of day (Sanksrit _prahar_). The audio itself is retrieved using the <a href="https://developers.google.com/youtube/iframe_api_reference" target="_blank"> YouTube IFrame Player API</a>.
+
+**_Ragas_ used**:
+
+**Happy or Excited, Morning**: 
+**Sad or Longing, Morning**: _Raga Todi_ - <a href="https://www.youtube.com/watch?v=sCr9vFdkI7Y"> Nikhil Banerjee (_Morning Ragas, Bombay_, 1965)</a>
+**Calm or Contemplative, Morning**: <a href ="https://www.youtube.com/watch?v=toBbAcTzGPM"> _Raga Bhairav_ - Ravi Shankar (_A Morning Raga/An Evening Raga_, 1968)</a>
+
+**Happy or Excited, Afternoon**: 
+**Sad or Longing, Afternoon**:
+**Calm or Contemplative, Afternoon**:
+
+**Happy or Excited, Night**: 
+**Sad or Longing, Night**:
+**Calm or Contemplative, Night**:
--- app.js
+++ app.js
@@ -1,24 +1,113 @@
-const moodButtons = document.querySelectorAll(".mood-buttons")
+const pageBackground = document.querySelector("body")
 
+// Mood buttons
+const moodButtons = document.querySelectorAll(".mood-buttons")
+const happyExcitedButton = document.querySelector("#happy-excited")
+const sadLongingButton = document.querySelector("#sad-longing")
+const calmContemplativeButton = document.querySelector("#calm-contemplative")
+
+// Time bßuttons
+const morningButton = document.querySelector("#morning")
+const afternoonButton = document.querySelector("#afternoon")
+const nightButton = document.querySelector("#night")
+
+
+// Mood button clicking and disabling
 moodButtons.forEach(button => {
    button.addEventListener("click", () => {
       moodButtons.forEach (btn => {
          btn.disabled = true;
-      });
-      button.style.backgroundColor = "beige"
+         button.style.backgroundColor = "beige"
+      })
    })
 })
 
 const timeButtons = document.querySelectorAll(".time-buttons")
 
+// Time button clicking and disabling
 timeButtons.forEach(button => {
    button.addEventListener("click", () => {
       timeButtons.forEach (btn => {
          btn.disabled = true;
-      });
-      button.style.backgroundColor = "beige"
+         button.style.backgroundColor = "beige"
+         // if (morningButton.disabled === true && happyExcitedButton.disabled === true) {
+         //    // add click counter instead of true false
+         //    pageBackground.classList.add("happy-excited-morning");
+         }
+      )}
+   )})
+
+
+// Coding functionality of button clicks
+const happyExcitedBackground = () => {
+   happyExcitedButton.addEventListener("click", () => {
+      morningButton.addEventListener("click", () => {
+         pageBackground.classList.add("happy-excited-morning")
+      })
+      afternoonButton.addEventListener("click", () => {
+         pageBackground.classList.add("happy-excited-afternoon")
+      })
+      nightButton.addEventListener("click", () => {
+         pageBackground.classList.add("happy-excited-night")
+         // playVideo("H4vQqQ__5SI")
+      })
    })
-})
+}
+happyExcitedBackground()
 
+const sadLongingBackground = () => {
+   sadLongingButton.addEventListener("click", () => {
+      morningButton.addEventListener("click", () => {
+         pageBackground.classList.add("sad-longing-morning")
+      })
+      afternoonButton.addEventListener("click", () => {
+         pageBackground.classList.add("sad-longing-afternoon")
+      })
+      nightButton.addEventListener("click", () => {
+         pageBackground.classList.add("sad-longing-night")
+         // playVideo("H4vQqQ__5SI")
+      })
+   })
+}
+sadLongingBackground()
 
+const calmContemplativeBackground = () => {
+   calmContemplativeButton.addEventListener("click", () => {
+      morningButton.addEventListener("click", () => {
+         pageBackground.classList.add("calm-contemplative-morning")
+      })
+      afternoonButton.addEventListener("click", () => {
+         pageBackground.classList.add("calm-contemplative-afternoon")
+      })
+      nightButton.addEventListener("click", () => {
+         pageBackground.classList.add("calm-contemplative-night")
+         // playVideo("H4vQqQ__5SI")
+      })
+   })
+}
+calmContemplativeBackground()
 
+// if "mood-button" and "time-button" clicked = true
+   // call api to return raga
+// const pageBackground = document.querySelector("body")
+
+// const happyExcitedButton = document.querySelector("#happy-excited")
+// let happyCalmClicked = false
+
+// const morningButton = document.querySelector("#morning")
+// let morningClicked = false
+
+// window.onload = happyExcitedMorning = () => {
+//    happyExcitedButton.addEventListener("click", () => {
+//       happyExcitedClicked = true
+//    })
+//    morningButton.addEventListener("click" = () => {
+//       morningClicked = true
+//    })
+//    if (happyExcitedClicked == true && morningClicked == true) {
+//    // happyExcitedButton.addEventListener("click", () => {
+//       alert("works")
+//       pageBackground.classList.add("happy-excited-morning")
+//    }
+// }
+   // if (happyExcitedButton.disabled === false && morningButton.disabled === false) {   
