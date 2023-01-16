db9ca09632acf76ec191c5cc6970c789ecf2ea99
s-laul <54870014+s-laul@users.noreply.github.com>
2023-01-16 17:22:19 -0500

    Alert for raga added

--- app.js
+++ app.js
@@ -41,14 +41,17 @@ timeButtons.forEach(button => {
 const happyExcited = () => {
    happyExcitedButton.addEventListener("click", () => {
       morningButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Desh");
          pageBackground.classList.add("happy-excited-morning")
          vidPlayer.nightButton.onclick = playVideo("7oQj5Qh5ScU")
       })
       afternoonButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Dhani");
          pageBackground.classList.add("happy-excited-afternoon")
          vidPlayer.nightButton.onclick = playVideo("tW6RWP54Tsg")
       })
       nightButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Yaman");
          pageBackground.classList.add("happy-excited-night")
          vidPlayer.nightButton.onclick = playVideo("I4LP3ziNTlU")
       })
@@ -60,13 +63,16 @@ const sadLonging = () => {
    sadLongingButton.addEventListener("click", () => {
       morningButton.addEventListener("click", () => {
          pageBackground.classList.add("sad-longing-morning")
+         alert("Your raga is: Raga Todi");
          vidPlayer.nightButton.onclick = playVideo("IfA7ZeIZv78")
       })
       afternoonButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Multani");
          pageBackground.classList.add("sad-longing-afternoon")
          vidPlayer.nightButton.onclick = playVideo("f5JARal8cL4")
       })
       nightButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Darbari");
          pageBackground.classList.add("sad-longing-night")
          vidPlayer.nightButton.onclick = playVideo("TGQ6LIB8Z6E")
       })
@@ -77,14 +83,17 @@ sadLonging()
 const calmContemplative = () => {
    calmContemplativeButton.addEventListener("click", () => {
       morningButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Nat Bhairavi");
          pageBackground.classList.add("calm-contemplative-morning")
          vidPlayer.nightButton.onclick = playVideo("bttszaG-fZU")
       })
       afternoonButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Nat Bhimpalasi");
          pageBackground.classList.add("calm-contemplative-afternoon")
          vidPlayer.nightButton.onclick = playVideo("SuNEIYlw9z0")
       })
       nightButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Malkauns");
          pageBackground.classList.add("calm-contemplative-night")
          vidPlayer.nightButton.onclick = playVideo("ydQWGnMIpq0")
       })
@@ -97,14 +106,17 @@ calmContemplative()
 const morning = () => {
    morningButton.addEventListener("click", () => {
       happyExcitedButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Desh");
          pageBackground.classList.add("happy-excited-morning")
          vidPlayer.nightButton.onclick = playVideo("7oQj5Qh5ScU")
       })
       sadLongingButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Todi");
          pageBackground.classList.add("sad-longing-morning")
          vidPlayer.nightButton.onclick = playVideo("IfA7ZeIZv78")
       })
       calmContemplativeButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Nati Bhairavi");
          pageBackground.classList.add("calm-contemplative-morning")
          vidPlayer.nightButton.onclick = playVideo("bttszaG-fZU")
       })
@@ -115,14 +127,17 @@ morning()
 const afternoon = () => {
    afternoonButton.addEventListener("click", () => {
       happyExcitedButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Dhani");
          pageBackground.classList.add("happy-excited-afternoon")
          vidPlayer.nightButton.onclick = playVideo("tW6RWP54Tsg")
       })
       sadLongingButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Multani");
          pageBackground.classList.add("sad-longing-afternoon")
          vidPlayer.nightButton.onclick = playVideo("f5JARal8cL4")
       })
       calmContemplativeButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Bhimpalasi");
          pageBackground.classList.add("calm-contemplative-afternoon")
          vidPlayer.nightButton.onclick = playVideo("Meq8Fr7hvF0")
       })
@@ -133,14 +148,17 @@ afternoon()
 const night = () => {
    nightButton.addEventListener("click", () => {
       happyExcitedButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Yaman");
          pageBackground.classList.add("happy-excited-night")
          vidPlayer.nightButton.onclick = playVideo("I4LP3ziNTlU")
       })
       sadLongingButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Darbari");
          pageBackground.classList.add("sad-longing-night")
          vidPlayer.nightButton.onclick = playVideo("TGQ6LIB8Z6E")
       })
       calmContemplativeButton.addEventListener("click", () => {
+         alert("Your raga is: Raga Malkauns");
          pageBackground.classList.add("calm-contemplative-night")
          vidPlayer.nightButton.onclick = playVideo("ydQWGnMIpq0")
       })
